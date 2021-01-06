15a46d41eb09a5464dd904af65e563827a6ec368
Jahna McNamara <jahna.mcnamara@gmail.com>
2021-01-05 23:57:03 -0600

    Extract out Photo component, have click update parent state (not correct when last photo)

---
+++ src/Photo.js
@@ -0,0 +1,11 @@
+function Photo(props) {      
+    return (
+    <img 
+        src={props.image.url}
+        alt="new"
+        onClick={props.handlePhotoClick}
+    />
+    )
+}
+
+export default Photo
--- src/PhotoGallery.js
+++ src/PhotoGallery.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
 
 class PhotoGallery extends Component {
     constructor(props) {
         super(props)
         this.state = {currentImage: 0}
     }
+
+    handlePhotoChange = () => {
+        this.setState(prevState => {
+           return {currentImage: prevState.currentImage + 1}
+        })
+    }
+
     render() {
         return (
             <div className="photo-gallery">
-                <img 
-                    src={this.props.images[this.state.currentImage].url}
-                    alt="new"
+                <Photo
+                    handlePhotoClick= {this.handlePhotoChange}
+                    image={this.props.images[this.state.currentImage]}
                 />
             </div>
         )
